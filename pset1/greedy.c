1cc0387e827f9f78a334f32227322a35227f0ce8
Braden Jonsson <braden337@gmail.com>
2016-09-04 16:00:00 +0000

    use mod in greedy

--- pset1/greedy.c
+++ pset1/greedy.c
@@ -2,7 +2,8 @@
 #import <math.h>
 #import <cs50.h>
 
-
+/*
+// version with subtraction
 int main() {
   
   float change;
@@ -16,16 +17,14 @@ int main() {
   
   int coins = 0;
   
-  // printf("Cents: %d\t Coins: %d\n", cents, coins);
-  
   while (cents > 0) {
     if (cents >= 25) {
       cents -= 25;
       coins++;
     }
     else if (cents >= 10) {
-    cents -= 10;
-    coins++;
+      cents -= 10;
+      coins++;
     }
     else if (cents >= 5) {
       cents -= 5;
@@ -35,11 +34,31 @@ int main() {
       cents -= 1;
       coins++;
     }
-    
-    // printf("Cents: %d\t Coins: %d\n", cents, coins);
-    
   }
   
-  printf("%d\n", coins);
+  printf("Cents: %d\t Coins: %d\n", cents, coins);
+  
+}
+*/
+
+int main() {
+  
+  float change;
+  
+  do {
+    printf("Your change in dollars: ");
+    change = GetFloat();
+  } while (change < 0);
+  
+  int balance = round(change*100);
+  int coin[] = {25, 10, 5, 1};
+  int coin_count = 0;
+  
+  for (int i = 0; balance > 0; i++) {
+    coin_count += balance / coin[i];
+    balance %= coin[i];
+  }
+  
+  printf("Minimum coins for $%.2f change is %i.\n", change, coin_count);
   
 }
